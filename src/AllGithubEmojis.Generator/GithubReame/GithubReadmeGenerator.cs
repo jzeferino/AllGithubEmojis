a7452b37da956b044d70a96b128bfb9f340dbe26
jzeferino <jorgevalentzeferino@gmail.com>
2017-10-29 22:27:05 +0000

    Commit the file with autosave identation.

--- src/AllGithubEmojis.Generator/GithubReame/GithubReadmeGenerator.cs
+++ src/AllGithubEmojis.Generator/GithubReame/GithubReadmeGenerator.cs
@@ -6,110 +6,110 @@ using AllGithubEmojis.Core.Model;
 
 namespace AllGithubEmojis.Generator.GithubReadme
 {
-	public class GithubReadmeGenerator
-	{
-		private StringBuilder _builder;
-		private readonly int _colums;
-		private Emojis _emojis;
+    public class GithubReadmeGenerator
+    {
+        private StringBuilder _builder;
+        private readonly int _colums;
+        private Emojis _emojis;
 
-		private GithubReadmeGenerator(Emojis emojis, int colums)
-		{
-			_builder = new StringBuilder();
-			_colums = colums;
-			_emojis = emojis;
-		}
+        private GithubReadmeGenerator(Emojis emojis, int colums)
+        {
+            _builder = new StringBuilder();
+            _colums = colums;
+            _emojis = emojis;
+        }
 
-		public static string Generate(Emojis emojis, int colums) => new GithubReadmeGenerator(emojis, colums).Generate();
+        public static string Generate(Emojis emojis, int colums) => new GithubReadmeGenerator(emojis, colums).Generate();
 
-		private string Generate()
-		{
-			CreateHeaderDescription();
-			CreateIndex();
-			CreateTables();
+        private string Generate()
+        {
+            CreateHeaderDescription();
+            CreateIndex();
+            CreateTables();
 
-			return _builder.ToString();
-		}
+            return _builder.ToString();
+        }
 
-		private void CreateIndex()
-		{
-			foreach (var emojiGroup in _emojis.Groups.Where(g => g.SubGroups.Count > 0))
-			{
-				_builder.AppendLine($"* [{emojiGroup.Name}](#{emojiGroup.Name.ToLower().Replace(" & ", "--")})");
-				foreach (var subGroup in emojiGroup.SubGroups.Where(s => s.Emojis.Count > 0))
-				{
-					_builder.AppendLine($"\t * [{subGroup.Name}](#{subGroup.Name.ToLower().Replace(" & ", "--")})");
-				}
-			}
-		}
+        private void CreateIndex()
+        {
+            foreach (var emojiGroup in _emojis.Groups.Where(g => g.SubGroups.Count > 0))
+            {
+                _builder.AppendLine($"* [{emojiGroup.Name}](#{emojiGroup.Name.ToLower().Replace(" & ", "--")})");
+                foreach (var subGroup in emojiGroup.SubGroups.Where(s => s.Emojis.Count > 0))
+                {
+                    _builder.AppendLine($"\t * [{subGroup.Name}](#{subGroup.Name.ToLower().Replace(" & ", "--")})");
+                }
+            }
+        }
 
-		private void CreateTables()
-		{
-			foreach (var emojiGroup in _emojis.Groups)
-			{
-				_builder.AppendLine($"# {emojiGroup.Name}");
-				foreach (var subGroup in emojiGroup.SubGroups)
-				{
-					CreateEmojiTable(subGroup);
-				}
-			}
-		}
+        private void CreateTables()
+        {
+            foreach (var emojiGroup in _emojis.Groups)
+            {
+                _builder.AppendLine($"# {emojiGroup.Name}");
+                foreach (var subGroup in emojiGroup.SubGroups)
+                {
+                    CreateEmojiTable(subGroup);
+                }
+            }
+        }
 
-		private void CreateHeaderDescription()
-		{
-			_builder.AppendLine("# [AllGithubEmojis](https://jzeferino.github.io/AllGithubEmojis/)");
+        private void CreateHeaderDescription()
+        {
+            _builder.AppendLine("# [AllGithubEmojis](https://jzeferino.github.io/AllGithubEmojis/)");
 
-			_builder.AppendLine("* A list of **all supported github** emojis updated weekly.");
-			_builder.AppendLine("* **Automatically generated** from github emoji API.");
-			_builder.AppendLine("* Grouped following official emoji [convention](http://unicode.org/emoji/charts/full-emoji-list.html).");
+            _builder.AppendLine("* A list of **all supported github** emojis updated weekly.");
+            _builder.AppendLine("* **Automatically generated** from github emoji API.");
+            _builder.AppendLine("* Grouped following official emoji [convention](http://unicode.org/emoji/charts/full-emoji-list.html).");
 
-			_builder.AppendFormat("## <p align=\"center\"><b>{0} Emojis available</b></p>\n", NumberToEmoji(_emojis.Count));
-			_builder.AppendFormat("### <p align=\"center\">Last updated (UTC) {0}</p>\n", DateTime.UtcNow);
-		}
+            _builder.AppendFormat("## <p align=\"center\"><b>{0} Emojis available</b></p>\n", NumberToEmoji(_emojis.Count));
+            _builder.AppendFormat("### <p align=\"center\">Last updated (UTC) {0}</p>\n", DateTime.UtcNow);
+        }
 
-		private string NumberToEmoji(int number)
-		{
-			string[] numbersToLetters = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private string NumberToEmoji(int number)
+        {
+            string[] numbersToLetters = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-			var builder = new StringBuilder();
+            var builder = new StringBuilder();
 
-			while (number > 0)
-			{
-				builder.Insert(0, string.Format(" :{0}:", numbersToLetters[number % 10]));
-				number = number / 10;
-			}
-			return builder.ToString();
-		}
+            while (number > 0)
+            {
+                builder.Insert(0, string.Format(" :{0}:", numbersToLetters[number % 10]));
+                number = number / 10;
+            }
+            return builder.ToString();
+        }
 
-		private void CreateEmojiTable(EmojiSubGroup emojiSubGroup)
-		{
-			AddTableTitle(emojiSubGroup);
-			AddTAbleContent(emojiSubGroup.Emojis);
-		}
+        private void CreateEmojiTable(EmojiSubGroup emojiSubGroup)
+        {
+            AddTableTitle(emojiSubGroup);
+            AddTAbleContent(emojiSubGroup.Emojis);
+        }
 
-		private void AddTAbleContent(List<Emoji> emojis)
-		{
-			var colums = Math.Min(emojis.Count, _colums);
-			for (int i = 0; i < emojis.Count; i++)
-			{
-				if (i % colums == 0 && i > 0)
-				{
-					_builder.AppendLine("|");
-				}
-				var emoji = emojis.ElementAt(i);
-				_builder.AppendFormat("| :{0}: **`:{1}:`** ", emoji.Code, emoji.Code);
+        private void AddTAbleContent(List<Emoji> emojis)
+        {
+            var colums = Math.Min(emojis.Count, _colums);
+            for (int i = 0; i < emojis.Count; i++)
+            {
+                if (i % colums == 0 && i > 0)
+                {
+                    _builder.AppendLine("|");
+                }
+                var emoji = emojis.ElementAt(i);
+                _builder.AppendFormat("| :{0}: **`:{1}:`** ", emoji.Code, emoji.Code);
 
-				if (i == (colums - 1))
-				{
-					_builder.AppendLine();
-					_builder.Insert(_builder.Length, "|:---:", colums);
-				}
-			}
-			_builder.AppendLine();
-		}
+                if (i == (colums - 1))
+                {
+                    _builder.AppendLine();
+                    _builder.Insert(_builder.Length, "|:---:", colums);
+                }
+            }
+            _builder.AppendLine();
+        }
 
-		private void AddTableTitle(EmojiSubGroup subgroup)
-		{
-			_builder.AppendLine($"### {subgroup.Name}");
-		}
-	}
+        private void AddTableTitle(EmojiSubGroup subgroup)
+        {
+            _builder.AppendLine($"### {subgroup.Name}");
+        }
+    }
 }
